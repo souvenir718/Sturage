a7c6e46d0a9f19b55a182f4b46423e12e8216476
DESKTOP-2L5NM4J\user <mongkey0@naver.com>
2020-09-05 22:26:46 +0900

    [0905] GroupList 페이지 관련 스토어 작성, 컨테이너로 연결

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import MainPage from "./main/MainPage";
-import GroupListPage from "./groupList/GroupListPage";
 import GroupInfoPage from "./groupInfo/GroupInfoPage";
 import Header from "./header/Header";
 import { Route } from "react-router-dom";
@@ -9,6 +8,7 @@ import Footer from "./footer/Footer";
 import GroupDetailPage from "./groupDetail/GroupDetailPage";
 import AddGroup from "./user/view/addGroup";
 import SignUpPage from "./user/view/signUp";
+import GroupListContainer from "./groupList/container/GroupListContainer";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
       <Header />
       <Route path="/" component={MainPage} exact={true} />
       <Route path="/signUp" component={SignUpPage} exact={true} />
-      <Route path="/list" component={GroupListPage} exact={true} />
+      <Route path="/list" component={GroupListContainer} exact={true} />
       <Route path="/info" component={GroupInfoPage} exact={true} />
       <Route path="/detail" component={GroupDetailPage} exact={true} />
       <Route path="/addGroup" component={AddGroup} exact={true} />
