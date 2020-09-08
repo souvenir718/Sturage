cf2cd9a7030a6cd3b8e34ceb82dde8449c33c16a
DESKTOP-2L5NM4J\user <mongkey0@naver.com>
2020-09-08 22:55:38 +0900

    [09080] GroupLeader 디테일 작성 진행 중

--- src/Common/token.js
+++ src/Common/token.js
@@ -1,135 +1,135 @@
-import axios from "axios";
-/*
-  예시 ) User 로그인 요청
-  1. 보낼 경로(로그인 요청)와 경로에 맞는 데이터를 세팅한다.
-  2. 요청을 보낸다.
-  3. 예외처리
-*/
-const userLogin = async () => {
-  // 보낼 경로
-  const api = "/api/login/";
-  // 보낼 데이터 이부분은 커스텀하며될듯
-  const data = {
-    username: "test",
-    password: "12345",
-  };
+// import axios from "axios";
+// /*
+//   예시 ) User 로그인 요청
+//   1. 보낼 경로(로그인 요청)와 경로에 맞는 데이터를 세팅한다.
+//   2. 요청을 보낸다.
+//   3. 예외처리
+// */
+// const userLogin = async () => {
+//   // 보낼 경로
+//   const api = "/api/login/";
+//   // 보낼 데이터 이부분은 커스텀하며될듯
+//   const data = {
+//     username: "test",
+//     password: "12345",
+//   };
 
-  // POST요청 (지금은 로그인 요청)
-  return await axios
-    .post(api, data)
-    .then((res) => {
-      //성공
-      // 로그인시 토큰 취득
-      const jwt_token = res.data.token;
-      // localStorage에 accessToken으로 세팅
-      localStorage.accessToken = jwt_token;
+//   // POST요청 (지금은 로그인 요청)
+//   return await axios
+//     .post(api, data)
+//     .then((res) => {
+//       //성공
+//       // 로그인시 토큰 취득
+//       const jwt_token = res.data.token;
+//       // localStorage에 accessToken으로 세팅
+//       localStorage.accessToken = jwt_token;
 
-      /*
-      앞으로 axios요청을 보낼때마다 발급받은 토큰을 헤더에 넣어서 보낼 수 있도록 세팅
-      - 페이지가 리로딩되면 초기화 됨
-      */
-      axios.defaults.headers.common["Authorization"] = `JWT ${jwt_token}`;
+//       /*
+//       앞으로 axios요청을 보낼때마다 발급받은 토큰을 헤더에 넣어서 보낼 수 있도록 세팅
+//       - 페이지가 리로딩되면 초기화 됨
+//       */
+//       axios.defaults.headers.common["Authorization"] = `JWT ${jwt_token}`;
 
-      // 이후에 페이지를 이동시키거나, 데이터를 가지고 작업을 진행하면됨
-    })
-    .catch(function (error) {
-      // 기본적인 인증 오류 메세지
-      // mssage: "Signature has expired."                        => 토큰 인증시간 만료 (로그인을 다시하도록 유도)
-      // mssage: "Authentication credentials were not provided." => 토큰 인증에러 (발급받은 토큰이 유효하거나, 토큰을보내지않음)
+//       // 이후에 페이지를 이동시키거나, 데이터를 가지고 작업을 진행하면됨
+//     })
+//     .catch(function (error) {
+//       // 기본적인 인증 오류 메세지
+//       // mssage: "Signature has expired."                        => 토큰 인증시간 만료 (로그인을 다시하도록 유도)
+//       // mssage: "Authentication credentials were not provided." => 토큰 인증에러 (발급받은 토큰이 유효하거나, 토큰을보내지않음)
 
-      if (error.response) {
-        const err = {
-          header: error.response.headers,
-          code: error.response.status,
-          mssage: error.response.data.detail,
-        };
-        console.log(err);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log("Error", error.message);
-      }
-    });
-};
-
-// 화면을 갱신할때 이 로컬 스토리지에 저장된 토큰을 axios 헤더에 설정하는 로직도 추가한다.
-const enhanceAccessToeken = () => {
-  const { accessToken } = localStorage;
-  if (!accessToken) return;
-  axios.defaults.headers.common["Authorization"] = `JWT ${accessToken}`;
-};
-
-// const getUser = () => {
-//   const api = "/api/user/";
-
-//   if (localStorage.accessToken) {
-//     axios.get(api);
-//   } else {
-//     //login 으로
-//   }
+//       if (error.response) {
+//         const err = {
+//           header: error.response.headers,
+//           code: error.response.status,
+//           mssage: error.response.data.detail,
+//         };
+//         console.log(err);
+//       } else if (error.request) {
+//         console.log(error.request);
+//       } else {
+//         console.log("Error", error.message);
+//       }
+//     });
 // };
 
-const getUsers = () => {
-  const api = "/api/users/";
+// // 화면을 갱신할때 이 로컬 스토리지에 저장된 토큰을 axios 헤더에 설정하는 로직도 추가한다.
+// const enhanceAccessToeken = () => {
+//   const { accessToken } = localStorage;
+//   if (!accessToken) return;
+//   axios.defaults.headers.common["Authorization"] = `JWT ${accessToken}`;
+// };
 
-  axios
-    .get(api)
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch(function (error) {
-      if (error.response) {
-        // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
-        const err = {
-          header: error.response.headers,
-          code: error.response.status,
-          mssage: error.response.data.detail,
-        };
-        console.log(err);
-      } else if (error.request) {
-        // 요청이 이루어 졌으나 응답을 받지 못했습니다.
-        // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
-        // Node.js의 http.ClientRequest 인스턴스입니다.
-        console.log(error.request);
-      } else {
-        // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
-        console.log("Error", error.message);
-      }
-      // console.log(error.config);
-    });
-};
+// // const getUser = () => {
+// //   const api = "/api/user/";
 
-const getGroups = () => {
-  const api = "/api/groups/";
-  axios
-    .get(api)
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch(function (error) {
-      if (error.response) {
-        const err = {
-          header: error.response.headers,
-          code: error.response.status,
-          mssage: error.response.data.detail,
-        };
-        console.log(err);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log("Error", error.message);
-      }
-      console.log(error.config.detail);
-    });
-};
+// //   if (localStorage.accessToken) {
+// //     axios.get(api);
+// //   } else {
+// //     //login 으로
+// //   }
+// // };
 
-userLogin().then((res) => {
-  // 로그인이 된 후에 실행하도록
-  console.log(res);
-  getUsers();
-  getGroups();
-});
+// const getUsers = () => {
+//   const api = "/api/users/";
 
-// enhanceAccessToeken();
+//   axios
+//     .get(api)
+//     .then((res) => {
+//       console.log(res.data);
+//     })
+//     .catch(function (error) {
+//       if (error.response) {
+//         // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
+//         const err = {
+//           header: error.response.headers,
+//           code: error.response.status,
+//           mssage: error.response.data.detail,
+//         };
+//         console.log(err);
+//       } else if (error.request) {
+//         // 요청이 이루어 졌으나 응답을 받지 못했습니다.
+//         // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
+//         // Node.js의 http.ClientRequest 인스턴스입니다.
+//         console.log(error.request);
+//       } else {
+//         // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
+//         console.log("Error", error.message);
+//       }
+//       // console.log(error.config);
+//     });
+// };
 
-// export { userLogin, enhanceAccessToeken, getUsers, getGroups };
+// const getGroups = () => {
+//   const api = "/api/groups/";
+//   axios
+//     .get(api)
+//     .then((res) => {
+//       console.log(res.data);
+//     })
+//     .catch(function (error) {
+//       if (error.response) {
+//         const err = {
+//           header: error.response.headers,
+//           code: error.response.status,
+//           mssage: error.response.data.detail,
+//         };
+//         console.log(err);
+//       } else if (error.request) {
+//         console.log(error.request);
+//       } else {
+//         console.log("Error", error.message);
+//       }
+//       console.log(error.config.detail);
+//     });
+// };
+
+// userLogin().then((res) => {
+//   // 로그인이 된 후에 실행하도록
+//   console.log(res);
+//   getUsers();
+//   getGroups();
+// });
+
+// // enhanceAccessToeken();
+
+// // export { userLogin, enhanceAccessToeken, getUsers, getGroups };
--- src/GroupLeader/GroupLeader.js
+++ src/GroupLeader/GroupLeader.js
@@ -1,28 +1,136 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./GroupLeader.scss";
-import { Grid, Header, Icon, Button } from "semantic-ui-react";
+import { Grid, Header, Icon, Button, Input } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 class GroupLeader extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isAdd: false,
       isGroup: false,
+      subject: "",
+      subjectList: [
+        {
+          id: 0,
+          times: 1,
+          title: "First Week Subject",
+          isTodo: false,
+        },
+        {
+          id: 1,
+          times: 2,
+          title: "Second Week Subject",
+          isTodo: false,
+        },
+        {
+          id: 2,
+          times: 3,
+          title: "Third Week Subject",
+          isTodo: false,
+        },
+      ],
     };
+    this.numOfSubject = this.state.subjectList.length;
   }
+  getList = () => {
+    let dataList = this.state.subjectList;
+    let subjectList = dataList.map((data) => (
+      <Fragment key={data.id}>
+        <div className="leader-content">
+          <p className="content-header">{data.times}. Times</p>
+          <span
+            className="content-title"
+            onClick={() => this.onClickAddTodo(data.id)}
+          >
+            {data.title}
+          </span>
+
+          <Icon
+            name="minus square"
+            onClick={() => this.deleteSubject(data.id)}
+          />
+        </div>
+        {data.isTodo && <div>todo어뜨케하지</div>}
+      </Fragment>
+    ));
+
+    return subjectList;
+  };
+  onClickAddTodo = (id) => {
+    let dataList = this.state.subjectList;
+
+    let subjectList = dataList.map((data) =>
+      data.id === id ? { ...data, isTodo: !data.isTodo } : data
+    );
+
+    this.setState({
+      subjectList: subjectList,
+    });
+  };
+  addSubject = (newSubject) => {
+    let dataList = this.state.subjectList;
+
+    let newData = {
+      id: this.numOfSubject,
+      times: this.numOfSubject + 1,
+      title: newSubject,
+    };
+    this.numOfSubject++;
+
+    dataList.push(newData);
+
+    this.setState({
+      subjectList: dataList,
+    });
+  };
+
+  onClickAddBtn = () => {
+    let isAdd = this.state.isAdd;
+    let newSubject = this.state.subject;
+    if (!isAdd) {
+      this.setState({
+        isAdd: !isAdd,
+      });
+    } else {
+      this.addSubject(newSubject);
+      this.setState({
+        isAdd: !isAdd,
+      });
+    }
+  };
+
+  deleteSubject = (id) => {
+    let dataList = this.state.subjectList;
+
+    dataList = dataList.filter((data) => data.id !== id);
+
+    this.setState({
+      subjectList: dataList,
+    });
+  };
+
   showGroup = () => {
     let isGroup = this.state.isGroup;
     this.setState({
       isGroup: !isGroup,
     });
   };
+  changeSubject = (e) => {
+    let newSubject = e.target.value;
+    this.setState({
+      subject: newSubject,
+    });
+  };
+
   render() {
+    let subjectList = this.getList();
     return (
       <>
         <div className="leader-sidebar">
           <div className="sidebar-content">
             <div className="sidebar-group">
-              <Header onClick={this.showGroup}>
-                A Group <Icon name="angle down" />
+              <Header>
+                A Group <Icon name="angle down" onClick={this.showGroup} />
               </Header>
               {this.state.isGroup && (
                 <ul className="group-li">
@@ -63,33 +171,17 @@ class GroupLeader extends Component {
           <Grid.Row centered>
             <Grid.Column width={8} className="leader-header">
               <Header as="h1">A Group</Header>
-              <Icon name="plus square" />
+              <Icon name="plus square" onClick={this.onClickAddBtn} />
+              {this.state.isAdd && (
+                <div className="add-todo">
+                  <span>Subject : </span>
+                  <Input onChange={(e) => this.changeSubject(e)} />
+                </div>
+              )}
             </Grid.Column>
           </Grid.Row>
           <Grid.Row centered className="leader-contents">
-            <Grid.Column width={8}>
-              <div className="leader-content">
-                <p className="content-header">1.Times</p>
-                <Link className="content-title">First Week Subject</Link>
-
-                <Icon name="minus square" />
-              </div>
-              <div className="leader-content">
-                <p className="content-header">2.Times</p>
-                <Link className="content-title">Second Week Subject</Link>
-                <Icon name="minus square" />
-              </div>
-              <div className="leader-content">
-                <p className="content-header">3.Times</p>
-                <Link className="content-title">Third Week Subject</Link>
-                <Icon name="minus square" />
-              </div>
-              <div className="leader-content">
-                <p className="content-header">4.Times</p>
-                <Link className="content-title">Fourth Week Subject</Link>
-                <Icon name="minus square" />
-              </div>
-            </Grid.Column>
+            <Grid.Column width={8}>{subjectList}</Grid.Column>
           </Grid.Row>
         </Grid>
       </>
